fea2964a71e2be6fa61a9d93cca77ba9451e089a
Crocodile <wwwblogger1@gmail.com>
2023-02-08 19:34:26 +0200

    add Generics

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿
+
+
+namespace Lesson
+{
+    class program
+    {
+        public static void Main(string[] args)
+        {
+            int a = 1, b = 5;
+
+            Console.WriteLine($"a = {a}\t b = {b}");
+
+            Swap(ref a, ref b);
+
+            Console.WriteLine($"a = {a}\t b = {b}");
+        }
+
+        static void Swap(ref int a, ref int b)
+        {
+            int temp = a;
+            a = b;  
+            b = temp;
+        }
+
+       /* static void Swap(ref double a, ref double b)
+        {
+            double temp = a;
+            a = b;
+            b = temp;
+        }
+       */
+    }
+}
